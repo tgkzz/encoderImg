d870614dd2ce21a575eb8291e9bf85c3106afce5
tgkzz <lilkamavertttt@gmail.com>
2024-07-10 01:07:28 +0500

    qwecommit

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ import (
 )
 
 var (
-	url = "https://base64.guru/converter/encode/url"
+	url    = "https://base64.guru/converter/encode/url"
+	auth   = "Bearer key-2q3QDOy9GvwEmolkJp7OtTiiXOXDXwebzbDidj0cBJoU6MurfyeoB6XGSg7GDzi0hEGHjQJlLG9iMMOlHpe1W8iitQ8xNMal"
+	apiUrl = "https://api.getimg.ai/v1/stable-diffusion-xl/image-to-image"
 )
 
 type Req struct {
 	Link string `json:"link"`
+}
+
+type RespAI struct {
+	Url string `json:"url,omitempty"`
+}
+
+type ErrRepsAi struct {
 }
 
 func main() {
@@ -77,9 +86,41 @@ func SupportHandler(c echo.Context) error {
 
 	encodeImg := base64.StdEncoding.EncodeToString(d)
 
+	cli := resty.New()
+
+	body := map[string]interface{}{
+		"model":           "stable-diffusion-xl-v1-0",
+		"prompt":          "anime style picture",
+		"negative_prompt": "Disfigured, cartoon, blurry",
+		"image":           encodeImg,
+		"strength":        0.5,
+		"steps":           50,
+		"guidance":        7.5,
+		"seed":            1,
+		"scheduler":       "euler",
+		"output_format":   "jpeg",
+		"response_format": "url",
+	}
+
+	var rr RespAI
+	resp2, err := cli.R().
+		SetHeaders(map[string]string{"Authorization": auth}).
+		SetBody(body).
+		SetResult(&rr).
+		Post(apiUrl)
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err)
+	}
+
+	if resp2.IsError() {
+		return c.JSON(http.StatusUnprocessableEntity, err)
+	}
+
 	return c.JSON(http.StatusOK, struct {
-		Base64 string `json:"base64,omitempty"`
+		Url    string `json:"url,omitempty"`
+		Base64 string `json:"base64,omitempty" json:"base64,omitempty"`
 	}{
+		Url:    rr.Url,
 		Base64: encodeImg,
 	})
 }
